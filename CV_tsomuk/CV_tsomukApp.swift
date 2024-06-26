ca85d786301af78f29db3c417472ac1d894c62ca
Tsomuk Nikita <tsomuk@me.com>
2024-06-26 23:19:22 +0300

    Initial Commit

---
+++ CV_tsomuk/CV_tsomukApp.swift
@@ -0,0 +1,17 @@
+//
+//  CV_tsomukApp.swift
+//  CV_tsomuk
+//
+//  Created by Nikita Tsomuk on 26.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CV_tsomukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
